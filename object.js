659b639ddd449690bd75983009dfa3f950b03725
anisfikriyyah <anisfikri3@gmail.com>
2020-11-13 23:58:20 +0700

    namabahin objek

---
+++ object.js
@@ -0,0 +1,16 @@
+const anis = {
+  nama: "anis",
+  alamat: {
+    desa: "curug",
+    rt: "05",
+    rw: "04",
+  },
+  birthday: {
+    day: "19-12-1994",
+    place: "Bandung",
+  },
+  hobby: ["makan", "nonton", "main HP"],
+};
+
+console.log(anis.hobby[2]);
+console.log(anis.alamat.desa);
